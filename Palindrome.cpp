205e19d492850a2da6eee4ded99468d39d4f7a72
ha1domo534 <danil.ionin.84@mail.ru>
2025-03-12 23:25:21 +0700

    Обновлена функция palindrome

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+
 bool palindrome(const char str[]) {
     int length = 0;
     // Вычисляем длину строки
@@ -10,8 +11,17 @@ bool palindrome(const char str[]) {
     int left = 0;
     int right = length - 1;
 
-    // Сравниваем символы с начала и конца
+    // Сравниваем символы с начала и конца, игнорируя пробелы
     while (left < right) {
+        // Пропускаем пробелы слева
+        while (left < right && str[left] == ' ') {
+            left++;
+        }
+        // Пропускаем пробелы справа
+        while (left < right && str[right] == ' ') {
+            right--;
+        }
+        // Сравниваем символы
         if (str[left] != str[right]) {
             return false;
         }
@@ -25,12 +35,12 @@ int main() {
     char str[100]; // Массив для хранения строки
 
     cout << "Введите строку: ";
-    cin >> str; // Считываем строку
+    cin.getline(str, 100); // Считываем строку с пробелами
 
     if (palindrome(str)) {
-        cout << "Строка является палиндромом." <<endl;
+        cout << "Строка является палиндромом." << endl;
     } else {
-        cout << "Строка не является палиндромом." <<endl;
+        cout << "Строка не является палиндромом." << endl;
     }
 
     return 0;
